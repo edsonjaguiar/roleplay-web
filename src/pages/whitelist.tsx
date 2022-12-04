7d8b2356b45a1ba8f9331f4f39394fe6c9bdb0be
edsonjaguiar <edsonaj09@gmail.com>
2022-12-04 17:26:54 -0300

    chore: Aligning the white list route text in the center of the page

--- src/pages/whitelist.tsx
+++ src/pages/whitelist.tsx
@@ -1,7 +1,7 @@
 export default function WhiteList() {
     return (
-        <div className="flex items-center justify-center">
-            <h1>Ainda sendo feito 🤖</h1>
+        <div className="flex w-full h-[100vh] items-center justify-center">
+            <h1 className="text-2xl font-medium">Ainda sendo feito 🤖</h1>
         </div>
     );
 }
